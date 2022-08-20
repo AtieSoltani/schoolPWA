c2dde9197817b44c43ef85e20bedc7366da0bb3f
atie <atiye.soltani77@gmail.com>
2022-08-20 21:02:44 +0430

    set user layout

---
+++ layouts/user.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <app-bar></app-bar>
+    <div class="mt-8"></div>
+    <Nuxt/>
+
+  </v-app>
+</template>
+
+<script>
+import AppBar from "~/components/Navigation/AppBar";
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    AppBar
+  },
+  data() {
+    return {}
+  }
+}
+</script>
