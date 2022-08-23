2a0377e21652a6ea89fead4914c1c743873bfcb1
atie <atiye.soltani77@gmail.com>
2022-08-23 11:13:32 +0430

    design basic information component

---
+++ components/Home/BasicInformation.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="d-sm-flex justify-space-around">
+
+    <div class="basic-info my-4 d-flex justify-space-around flex-grow-1">
+
+      <div class="d-flex flex-column align-center ">
+
+        <svg class="basic-info-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink"
+             x="0px"
+             y="0px"
+             viewBox="0 0 459.863 459.863" style=":new 0 0 459.863 459.863;" xml:space="preserve">
+<g>
+	<g>
+		<g>
+			<path d="M395.988,193.977c-18.629,24.992-45.641,41.956-77.406,44.25c26.626,31.04,43.84,70.378,47.152,113.55
+				c1.243,16.197,0.369,24.083-0.932,30.599c16.064-2.497,30.815-6.128,43.488-10.794c42.626-15.694,51.573-38.891,51.573-55.586
+				C459.863,265.521,434.565,220.849,395.988,193.977z"/>
+      <path d="M276.72,199.407c10.642,5.952,22.36,9.093,34.525,9.093c45.83,0,81.115-44.3,81.115-96.677
+				c0-52.382-35.29-96.677-81.115-96.677c-18.734,0-36.411,7.436-50.724,21.145c20.03,25.65,31.446,59.072,31.446,94.667
+				C291.967,154.544,286.961,178.037,276.72,199.407z"/>
+      <path d="M259.999,226.279c-24.558,31.058-57.739,46.482-91.839,46.482c-36.232,0-68.69-17.206-91.839-46.482
+				C30.403,256.333,0,308.217,0,367.076c0,18.127,9.926,43.389,57.213,60.8c29.496,10.861,68.898,16.841,110.947,16.841
+				c42.049,0,81.451-5.98,110.947-16.841c47.287-17.411,57.213-42.673,57.213-60.8C336.32,308.217,305.918,256.333,259.999,226.279z
+				"/>
+      <path d="M168.16,242.763c53.003,0,93.806-51.234,93.806-111.804c0-60.571-40.808-111.804-93.806-111.804
+				c-52.995,0-93.806,51.223-93.806,111.804C74.354,191.541,115.169,242.763,168.16,242.763z"/>
+		</g>
+	</g>
+</g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g>
+</g>
+</svg>
+
+        <div class="basic-info-number pt-2 font-weight-black">320</div>
+
+        <div class="basic-info-title font-weight-bold text--accent-3">دانش آموز</div>
+
+      </div>
+
+      <div class="d-flex flex-column align-center ">
+
+        <svg class="basic-info-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink"
+             x="0px"
+             y="0px"
+             width="256px" height="240px" viewBox="0 0 256 240" enable-background="new 0 0 256 240"
+             xml:space="preserve">
+<path d="M84.635,20.256c18.383,0,33.286,14.903,33.286,33.286s-14.903,33.286-33.286,33.286S51.349,71.925,51.349,53.542
+	S66.251,20.256,84.635,20.256z M31.002,145.011c0-2.499,1.606-4.194,4.194-4.194s4.194,1.606,4.194,4.194v92.986h91.469v-92.986
+	c0-2.499,1.606-4.194,4.194-4.194c2.499,0,4.194,1.606,4.194,4.194v92.986h29.092V136.623c0-22.934-18.74-41.585-41.585-41.585
+	h-8.388l-24.451,38.015l-2.945-28.467l4.016-9.638H76.96l4.016,9.638l-3.123,28.645L53.401,95.038h-9.816
+	C20.651,95.038,2,113.778,2,136.623v101.375h29.092v-92.986H31.002z M214,18h23v23h-23V18z M183,34h21v7h-21V34z M183,18h21v7h-21
+	V18z M183,67h54v7h-54V67z M183,51h54v7h-54V51z M183,83h54v7h-54V83z M166,1.755V106h88V2L166,1.755z M246.838,99H173V9h3.526
+	h6.849H247L246.838,99z"/>
+</svg>
+
+        <div class="basic-info-number pt-2 font-weight-black">50</div>
+
+        <div class="basic-info-title font-weight-bold text--accent-3">کادر مدرسه</div>
+
+      </div>
+
+    </div>
+
+    <div class="basic-info my-4 d-flex justify-space-around flex-grow-1">
+
+      <div class="d-flex flex-column align-center ">
+
+        <svg class="basic-info-icon" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <g>
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M12 22a9 9 0 1 1 0-18 9 9 0 0 1 0 18zm1-9V8h-2v7h5v-2h-3zM1.747 6.282l3.535-3.535 1.415 1.414L3.16 7.697 1.747 6.282zm16.97-3.535l3.536 3.535-1.414 1.415-3.536-3.536 1.415-1.414z"/>
+          </g>
+        </svg>
+
+        <div class="basic-info-number pt-2 font-weight-black">7:00</div>
+
+        <div class="basic-info-title font-weight-bold text--accent-3">شروع کار مدرسه</div>
+
+      </div>
+
+      <div class="d-flex flex-column align-center ">
+
+        <svg class="basic-info-icon" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="m20.292 6.708-3.01-3 1.412-1.417 3.01 3zM21.707 20.293l-2.287-2.288C20.409 16.563 21 14.837 21 13c0-4.878-4.121-9-9-9-1.838 0-3.563.59-5.006 1.581L5.91 4.496l.788-.79-1.416-1.412-.786.788-.789-.789-1.414 1.414 18 18 1.414-1.414zM17 14h-1.586l-2-2H17v2zm-6-6h2v3.586l-2-2V8zm1 14c1.658 0 3.224-.485 4.574-1.305L4.305 8.426A8.794 8.794 0 0 0 3 13c0 4.878 4.122 9 9 9z"/>
+        </svg>
+
+        <div class="basic-info-number pt-2 font-weight-black">14:30</div>
+
+        <div class="basic-info-title font-weight-bold text--accent-3">پایان کار مدرسه</div>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasicInformation"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.basic-info {
+  &-icon {
+    width: 60px;
+    height: 60px;
+    fill: $color-primary !important;
+  }
+
+  &-number {
+    font-size: 20px;
+  }
+}
+
+</style>
