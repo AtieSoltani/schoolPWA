18da557bcae295e401628a0771ac40c58c3d7730
atie <atiye.soltani77@gmail.com>
2022-08-23 11:15:53 +0430

    add components into index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,19 +4,31 @@
       <home-carousel/>
     </div>
     <services/>
+    <facilities/>
+    <majors/>
+    <latest-news/>
+    <basic-information/>
   </div>
 </template>
 
 <script>
 import HomeCarousel from "~/components/Home/HomeCarousel";
 import Services from "~/components/Home/Services";
+import Facilities from "~/components/Home/Facilities";
+import Majors from "~/components/Home/Majors";
+import LatestNews from "~/components/Home/LatestNews";
+import BasicInformation from "~/components/Home/BasicInformation";
 
 export default {
   layout: 'user',
   name: 'IndexPage',
   components: {
     HomeCarousel,
-    Services
+    Services,
+    Facilities,
+    Majors,
+    LatestNews,
+    BasicInformation
   }
 }
 </script>
